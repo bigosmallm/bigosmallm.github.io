08409bc414199ad1c7b02f17a9d7f5dcbf1abbb9
Om Muppirala <omuppirala@machinezone.com>
2015-06-19 13:46:20 -0700

    Add scale animation to map example Clean up code, add comments

--- flexjs/mapexample/AS3DOM.js
+++ flexjs/mapexample/AS3DOM.js
@@ -22,44 +22,149 @@ AS3DOM = function() {
 
 
 /**
+ * @private
+ * @type {HTMLDivElement}
+ */
+AS3DOM.prototype.title;
+
+
+/**
+ * @private
+ * @type {SVGElement}
+ */
+AS3DOM.prototype.map;
+
+
+/**
  * @expose
  */
 AS3DOM.prototype.start = function() {
-  var /** @type {HTMLDivElement} */ title = org_apache_flex_utils_Language.as(document.createElement("div"), HTMLDivElement, true);
-  title.innerHTML = "US Map";
-  document.body.appendChild(title);
-  var /** @type {SVGElement} */ map = org_apache_flex_utils_Language.as(document.createElementNS("http://www.w3.org/2000/svg", "svg"), SVGElement, true);
-  map.style.width = 1000 + "px";
-  map.style.height = 1000 + "px";
-  document.body.appendChild(map);
+  this.createTitle();
+  this.createMap();
+};
+
+
+/**
+ * Create title
+ * @private
+ */
+AS3DOM.prototype.createTitle = function() {
+  this.title = org_apache_flex_utils_Language.as(document.createElement("div"), HTMLDivElement, true);
+  this.title.innerHTML = "US Map";
+  document.body.appendChild(this.title);
+};
+
+
+/**
+ * Create svg elements for map
+ * Parse and render each state
+ * @private
+ */
+AS3DOM.prototype.createMap = function() {
+  this.map = org_apache_flex_utils_Language.as(document.createElementNS("http://www.w3.org/2000/svg", "svg"), SVGElement, true);
+  this.map.style.width = 1000 + "px";
+  this.map.style.height = 1000 + "px";
+  document.body.appendChild(this.map);
   var /** @type {Object} */ usmapCoords = new USMapCoords().usmap;
-  for (var /** @type {Object} */ state in usmapCoords) {
-    var /** @type {SVGPathElement} */ path = org_apache_flex_utils_Language.as(document.createElementNS("http://www.w3.org/2000/svg", "path"), SVGPathElement, true);
+  var /** @type {SVGPathElement} */ path;
+  for (var /** @type {string} */ state in usmapCoords) {
+    path = org_apache_flex_utils_Language.as(document.createElementNS("http://www.w3.org/2000/svg", "path"), SVGPathElement, true);
     path.setAttribute("d", usmapCoords[state]);
     path.setAttribute("fill", "#FF0000");
     path.setAttribute("opacity", Math.random());
-    path.addEventListener("mouseover", goog.bind(this.handleStateRollOver, this), false);
-    path.addEventListener("mouseout", goog.bind(this.handleStateRollOut, this), false);
-    map.appendChild(path);
+    path.addEventListener("mouseover", goog.bind(this.handleStateMouseOver, this), false);
+    path.addEventListener("mouseout", goog.bind(this.handleStateMouseOut, this), false);
+    path.addEventListener("click", goog.bind(this.handleStateClick, this), false);
+    this.map.appendChild(path);
   }
 };
 
 
 /**
+ * State mouseover handler
  * @private
  * @param {MouseEvent} event
  */
-AS3DOM.prototype.handleStateRollOver = function(event) {
+AS3DOM.prototype.handleStateMouseOver = function(event) {
   org_apache_flex_utils_Language.as(event.target, SVGPathElement, true).setAttribute("fill", "#0000FF");
 };
 
 
 /**
+ * State mouseout handler
  * @private
  * @param {MouseEvent} event
  */
-AS3DOM.prototype.handleStateRollOut = function(event) {
+AS3DOM.prototype.handleStateMouseOut = function(event) {
   org_apache_flex_utils_Language.as(event.target, SVGPathElement, true).setAttribute("fill", "#FF0000");
+};
+
+
+/**
+ * @private
+ * @type {SVGPathElement}
+ */
+AS3DOM.prototype.pathToAnimate;
+
+
+/**
+ * @private
+ * @type {number}
+ */
+AS3DOM.prototype.scaleValue = 1;
+
+
+/**
+ * State click handler
+ * Start animating
+ * @private
+ * @param {MouseEvent} event
+ */
+AS3DOM.prototype.handleStateClick = function(event) {
+  this.pathToAnimate = org_apache_flex_utils_Language.as(event.target, SVGPathElement, true);
+  this.animateScaleUp();
+};
+
+
+/**
+ * Increment scale of path element by 0.1 each frame
+ * @private
+ */
+AS3DOM.prototype.animateScaleUp = function() {
+  this.scaleValue += 0.1;
+  this.setScale(this.pathToAnimate, this.scaleValue);
+  if (this.scaleValue >= 1.5) {
+    this.animateScaleDown();
+  } else {
+    requestAnimationFrame(goog.bind(this.animateScaleUp, this));
+  }
+};
+
+
+/**
+ * Decrement scale of path element by 0.1 each frame
+ * @private
+ */
+AS3DOM.prototype.animateScaleDown = function() {
+  this.scaleValue -= 0.1;
+  this.setScale(this.pathToAnimate, this.scaleValue);
+  if (this.scaleValue > 1) {
+    requestAnimationFrame(goog.bind(this.animateScaleDown, this));
+  }
+};
+
+
+/**
+ * Apply scale transform; ensure element stays in place while scaling
+ * @private
+ * @param {SVGElement} element
+ * @param {*} scale
+ */
+AS3DOM.prototype.setScale = function(element, scale) {
+  var /** @type {SVGRect} */ boundingRect = this.pathToAnimate.getBBox();
+  var /** @type {number} */ centerX = boundingRect.x;
+  var /** @type {number} */ centerY = boundingRect.y;
+  element.setAttribute("transform", "translate(" + centerX + "," + centerY + ") scale(" + this.scaleValue + ") translate(" + -1 * centerX + "," + -1 * centerY + ")");
 };
 
 
