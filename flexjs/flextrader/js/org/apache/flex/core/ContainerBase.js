60025b6b02086b20f786e50e9b40bb5f16f28e3a
Om Muppirala <omuppirala@machinezone.com>
2015-10-01 02:51:44 +0200

    FlexJS Flex Trader example

---
+++ flexjs/flextrader/js/org/apache/flex/core/ContainerBase.js
@@ -0,0 +1,325 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ *  @fileoverview
+ *  @suppress {missingRequire}
+ */
+goog.provide('org.apache.flex.core.ContainerBase');
+
+goog.require('org.apache.flex.core.IChrome');
+goog.require('org.apache.flex.core.IMXMLDocument');
+goog.require('org.apache.flex.core.UIBase');
+
+
+
+/**
+ * @constructor
+ * @extends {org.apache.flex.core.UIBase}
+ */
+org.apache.flex.core.ContainerBase = function() {
+  this.mxmlProperties = null;
+  org.apache.flex.core.ContainerBase.base(this, 'constructor');
+
+  /**
+   * @private
+   * @type {boolean}
+   */
+  this.initialized_ = false;
+
+  /**
+   * @private
+   * @type {Array}
+   */
+  this.states_ = null;
+
+  /**
+   * @private
+   * @type {Array}
+   */
+  this.transitions_ = null;
+
+  /**
+   * @private
+   * @type {?String}
+   */
+  this.currentState_ = null;
+
+  this.document = this;
+  this.actualParent_ = this;
+
+};
+goog.inherits(org.apache.flex.core.ContainerBase,
+    org.apache.flex.core.UIBase);
+
+
+/**
+ * @export
+ */
+org.apache.flex.core.ContainerBase.prototype.mxmlContent = null;
+
+
+/**
+ * @export
+ * @type {Array}
+ */
+org.apache.flex.core.ContainerBase.prototype.mxmlDescriptor = null;
+
+
+/**
+ * @export
+ * @type {Array}
+ */
+org.apache.flex.core.ContainerBase.prototype.mxmlsd = null;
+
+
+/**
+ * @private
+ * @type {Object}
+ */
+org.apache.flex.core.ContainerBase.prototype.actualParent_ = null;
+
+
+/**
+ * @export
+ * @type {boolean}
+ */
+org.apache.flex.core.ContainerBase.prototype.supportsChrome = true;
+
+
+/**
+ * Metadata
+ *
+ * @type {Object.<string, Array.<Object>>}
+ */
+org.apache.flex.core.ContainerBase.prototype.FLEXJS_CLASS_INFO =
+    { names: [{ name: 'ContainerBase',
+                qName: 'org.apache.flex.core.ContainerBase'}] ,
+      interfaces: [org.apache.flex.core.IMXMLDocument]};
+
+
+/**
+ * @override
+ */
+org.apache.flex.core.ContainerBase.prototype.addedToParent = function() {
+  org.apache.flex.core.ContainerBase.base(this, 'addedToParent');
+
+  if (!this.initialized_) {
+    org.apache.flex.utils.MXMLDataInterpreter.generateMXMLInstances(this.document,
+        this, this.MXMLDescriptor);
+
+    this.dispatchEvent('initBindings');
+    this.dispatchEvent('initComplete');
+    this.initialized_ = true;
+  }
+//??  this.dispatchEvent('childrenAdded');
+};
+
+
+/**
+ * @export
+ * @param {Array} data The data for the attributes.
+ */
+org.apache.flex.core.ContainerBase.prototype.generateMXMLAttributes = function(data) {
+  org.apache.flex.utils.MXMLDataInterpreter.generateMXMLProperties(this, data);
+};
+
+
+/**
+ * @export
+ * @param {Object} doc The document.
+ * @param {Array} desc The descriptor data.
+ */
+org.apache.flex.core.ContainerBase.prototype.setMXMLDescriptor =
+    function(doc, desc) {
+  this.mxmlDescriptor = desc;
+  this.document = doc;
+};
+
+
+/**
+ * @expose
+ * @param {Object} parent The component to use as the parent of the children for the container.
+ */
+org.apache.flex.core.ContainerBase.prototype.setActualParent = function(parent) {
+  this.actualParent_ = parent;
+};
+
+
+/**
+ * @override
+ */
+org.apache.flex.core.ContainerBase.prototype.addElement = function(c, opt_dispatchEvent) {
+  if (opt_dispatchEvent === undefined)
+    opt_dispatchEvent = true;
+
+  if (this.supportsChromeChildren && org.apache.flex.utils.Language.is(c, org.apache.flex.core.IChrome)) {
+     //org.apache.flex.core.ContainerBase.base(this, 'addElement', c);
+     this.element.appendChild(c.positioner);
+     c.addedToParent();
+  }
+  else {
+     //this.actualParent.addElement(c);
+     this.actualParent.element.appendChild(c.positioner);
+     c.addedToParent();
+  }
+  if (opt_dispatchEvent)
+    this.dispatchEvent('childrenAdded');
+};
+
+
+/**
+ * @override
+ */
+org.apache.flex.core.ContainerBase.prototype.addElementAt = function(c, index, opt_dispatchEvent) {
+  if (opt_dispatchEvent === undefined)
+    opt_dispatchEvent = true;
+
+  if (this.supportsChromeChildren && org.apache.flex.utils.Language.is(c, org.apache.flex.core.IChrome)) {
+     //org.apache.flex.core.ContainerBase.base(this, 'addElementAt', c, index);
+     var children1 = this.internalChildren();
+     if (index >= children1.length) {
+       this.addElement(c);
+     } else {
+       this.element.insertBefore(c.positioner,
+           children1[index]);
+       c.addedToParent();
+     }
+   } else {
+     //this.actualParent.addElementAt(c, index);
+     var children2 = this.actualParent.internalChildren();
+     if (index >= children2.length) {
+       this.actualParent.element.appendChild(c.positioner);
+       c.addedToParent();
+     } else {
+       this.actualParent.element.insertBefore(c.positioner,
+           children2[index]);
+       c.addedToParent();
+     }
+   }
+  if (opt_dispatchEvent)
+    this.dispatchEvent('childrenAdded');
+};
+
+
+/**
+ * @override
+ */
+org.apache.flex.core.ContainerBase.prototype.getElementAt = function(index) {
+  var children = this.actualParent.internalChildren();
+  return children[index].flexjs_wrapper;
+};
+
+
+/**
+ * @param {Object} c The child element.
+ */
+org.apache.flex.core.ContainerBase.prototype.removeElement = function(c) {
+  if (this.supportsChromeChildren && org.apache.flex.utils.Language.is(c, org.apache.flex.core.IChrome)) {
+     this.element.removeChild(c.element);
+  }
+  else {
+     this.actualParent.element.removeChild(c.element);
+  }
+};
+
+
+/**
+ * @override
+ */
+org.apache.flex.core.ContainerBase.prototype.getElementIndex = function(c) {
+  var children;
+  if (this.supportsChromeChildren && org.apache.flex.utils.Language.is(c, org.apache.flex.core.IChrome)) {
+     children = this.internalChildren();
+  }
+  else {
+    children = this.actualParent.internalChildren();
+  }
+  var n = children.length;
+  for (var i = 0; i < n; i++)
+  {
+    if (children[i] == c.element)
+      return i;
+  }
+  return -1;
+};
+
+
+Object.defineProperties(org.apache.flex.core.ContainerBase.prototype, {
+    /** @export */
+    MXMLDescriptor: {
+        /** @this {org.apache.flex.core.ContainerBase} */
+        get: function() {
+            return this.mxmlDescriptor;
+        }
+    },
+    /** @export */
+    states: {
+        /** @this {org.apache.flex.core.ContainerBase} */
+        get: function() {
+            return this.states_;
+        },
+        /** @this {org.apache.flex.core.ContainerBase} */
+        set: function(s) {
+            this.states_ = s;
+            this.currentState_ = s[0].name;
+
+            if (org.apache.flex.core.ValuesManager.valuesImpl.getValue) {
+              /**
+               * @type {Function}
+               */
+              var impl = /** @type {Function} */ (org.apache.flex.core.ValuesManager.valuesImpl.
+                  getValue(this, 'iStatesImpl'));
+              // TODO: (aharui) check if bead already exists
+              this.addBead(new impl());
+            }
+        }
+    },
+    /** @export */
+    currentState: {
+        /** @this {org.apache.flex.core.ContainerBase} */
+        get: function() {
+             return this.currentState_;
+        },
+        /** @this {org.apache.flex.core.ContainerBase} */
+        set: function(s) {
+             var event = new org.apache.flex.events.ValueChangeEvent(
+                  'currentStateChange', false, false, this.currentState_, s);
+             this.currentState_ = s;
+             this.dispatchEvent(event);
+        }
+    },
+    /** @export */
+    transitions: {
+        /** @this {org.apache.flex.core.ContainerBase} */
+        get: function() {
+             return this.transitions_;
+        },
+        /** @this {org.apache.flex.core.ContainerBase} */
+        set: function(s) {
+           this.transitions_ = s;
+        }
+    },
+    /** @export */
+    actualParent: {
+        /** @this {org.apache.flex.core.ContainerBase} */
+        get: function() {
+             return this.actualParent_;
+        },
+        /** @this {org.apache.flex.core.ContainerBase} */
+        set: function(s) {
+             this.actualParent_ = s;
+        }
+    }
+});
---
+++ flexjs/flextrader/js/org/apache/flex/core/ListBase.js
@@ -0,0 +1,115 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ *  @fileoverview
+ *  @suppress {missingRequire}
+ */
+goog.provide('org.apache.flex.core.ListBase');
+
+goog.require('org.apache.flex.core.ContainerBase');
+
+
+
+/**
+ * @constructor
+ * @extends {org.apache.flex.core.ContainerBase}
+ */
+org.apache.flex.core.ListBase = function() {
+  org.apache.flex.core.ListBase.base(this, 'constructor');
+
+  /**
+   * @private
+   * @type {number}
+   */
+  this.selectedIndex_ = -1;
+};
+goog.inherits(org.apache.flex.core.ListBase,
+    org.apache.flex.core.ContainerBase);
+
+
+/**
+ * Metadata
+ *
+ * @type {Object.<string, Array.<Object>>}
+ */
+org.apache.flex.core.ListBase.prototype.FLEXJS_CLASS_INFO =
+    { names: [{ name: 'ListBase',
+                qName: 'org.apache.flex.core.ListBase' }] };
+
+
+/**
+ * @override
+ */
+org.apache.flex.core.ListBase.prototype.createElement = function() {
+  //  this.element = document.createElement('select');
+  //  goog.events.listen(this.element, 'change',
+  //      goog.bind(this.changeHandler, this));
+  this.element = document.createElement('div');
+  this.element.style.position = 'relative';
+  this.element.style.overflow = 'auto';
+  this.element.style.border = 'solid';
+  this.element.style.borderWidth = '1px';
+  this.element.style.borderColor = '#333333';
+  this.positioner = this.element;
+  this.element.flexjs_wrapper = this;
+
+  return this.element;
+};
+
+
+Object.defineProperties(org.apache.flex.core.ListBase.prototype, {
+    /** @export */
+    dataProvider: {
+        /** @this {org.apache.flex.core.ListBase} */
+        get: function() {
+            return this.model.dataProvider;
+        },
+        /** @this {org.apache.flex.core.ListBase} */
+        set: function(value) {
+            this.model.dataProvider = value;
+        }
+    },
+    /** @export */
+    selectedIndex: {
+        /** @this {org.apache.flex.core.ListBase} */
+        get: function() {
+            return this.model.selectedIndex;
+        },
+        /** @this {org.apache.flex.core.ListBase} */
+        set: function(value) {
+            this.model.selectedIndex = value;
+        }
+    },
+    /** @export */
+    selectedItem: {
+        /** @this {org.apache.flex.core.ListBase} */
+        get: function() {
+            return this.model.selectedItem;
+        },
+        /** @this {org.apache.flex.core.ListBase} */
+        set: function(value) {
+            this.model.selectedItem = value;
+        }
+    }
+});
+
+
+/**
+ * @protected
+ */
+org.apache.flex.core.ListBase.prototype.changeHandler =
+    function() {
+  this.dispatchEvent('change');
+};
