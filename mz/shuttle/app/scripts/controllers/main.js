0a006343a243eabeb28dac5dddc5c2fd5186cd70
Om Muppirala <omuppirala@machinezone.com>
2016-04-21 02:43:53 -0700

    Add 'request' feature

--- mz/shuttle/app/scripts/controllers/main.js
+++ mz/shuttle/app/scripts/controllers/main.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /**
  * @ngdoc function
  * @name webappApp.controller:MainCtrl
@@ -7,6 +5,7 @@
  * # MainCtrl
  * Controller of the webappApp
  */
+'use strict';
 angular.module('webappApp')
   .controller('MainCtrl', function ($scope,$timeout,uiGmapGoogleMapApi) {
 
@@ -19,7 +18,7 @@ angular.module('webappApp')
     $scope.map = {center: { latitude: 37.419472, longitude: -122.146881 },
                   zoom: 15,
                   markers: [],
-                  options:{icon: 'images/bus_icon.png'}
+                  options:{icon: 'images/shuttle_icon.png'}
     };
 
     $scope.stop = {};
@@ -29,48 +28,51 @@ angular.module('webappApp')
         show: false,
         templateURL:'scripts/templates/mz.stop.template.html',
         isIconVisibleOnClick: true,
-        id: 'stop_mz'
+        id: 'stop_mz',
+        parent: $scope
       };
 
     $scope.handleMZStopClose = function() {
 
     };
 
-    $scope.stop.mz.destination = '';
-
     $scope.stop.caltrain =
     {
       coords: {latitude: 37.428844, longitude: -122.141447},
       show: false,
-      templateURL:'scripts/templates/all.stop.template.html',
+      templateURL:'scripts/templates/caltrain.stop.template.html',
       isIconVisibleOnClick: true,
-      id: 'stop_caltrain'
+      id: 'stop_caltrain',
+      parent: $scope
     };
+
 
     $scope.handleCaltrainStopClose = function() {
 
     };
-
     $scope.stop.hanover =
     {
       coords: {latitude: 37.413233, longitude: -122.142615},
       show: false,
-      templateURL:'scripts/templates/all.stop.template.html',
+      templateURL:'scripts/templates/hanover.stop.template.html',
       isIconVisibleOnClick: true,
-      id: 'stop_hanover'
+      id: 'stop_hanover',
+      parent: $scope
     };
+
 
     $scope.handleHanoverStopClose = function() {
 
     };
-
     uiGmapGoogleMapApi.then(function(maps) {
       $scope.initRTMChannels();
     });
 
     $scope.shuttleChannel1;
+
     $scope.shuttleChannel2;
     $scope.shuttleChannel3;
+    $scope.shuttleRequestChannel;
     $scope.iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
 
     $scope.initRTMChannels = function() {
@@ -78,78 +80,114 @@ angular.module('webappApp')
       $scope.shuttleChannel1 = connection.createChannel('Shuttle-1');
       $scope.shuttleChannel1.on('data', $scope.handleShuttle1Data );
       $scope.shuttleChannel1.subscribe();
-
       //Shuttle 2
       $scope.shuttleChannel2 = connection.createChannel('Shuttle-2');
+
       $scope.shuttleChannel2.on('data', $scope.handleShuttle2Data );
       $scope.shuttleChannel2.subscribe();
-
       //Shuttle 3
       $scope.shuttleChannel3 = connection.createChannel('Shuttle-3');
+
       $scope.shuttleChannel3.on('data', $scope.handleShuttle3Data );
       $scope.shuttleChannel3.subscribe();
-    };
+      //Shuttle request channel
 
+      $scope.shuttleRequestChannel = connection.createChannel('Requests');
+
+
+    };
     $scope.location1;
+
     $scope.handleShuttle1Data = function(data)
     {
       $scope.location1 = data.body.messages[0];
-      $timeout($scope.updateMap,500);
+      $timeout($scope.updateShuttle1Map,500);
     };
-
     $scope.location2;
+
     $scope.handleShuttle2Data = function(data)
     {
       $scope.location2 = data.body.messages[0];
-      $timeout($scope.updateMap,500);
+      $timeout($scope.updateShuttle2Map,500);
     };
-
     $scope.location3;
+
     $scope.handleShuttle3Data = function(data)
     {
       $scope.location3 = data.body.messages[0];
-      $timeout($scope.updateMap,500);
+      $timeout($scope.updateShuttle3Map,500);
     };
+    $scope.shuttle1 = {id: Date.now()};
 
-    $scope.updateMap = function()
-    {
-
-      $scope.map.markers = [];
-
-      if($scope.location1) {
-        var marker1 = {
+    $scope.updateShuttle1Map = function() {
+      if ($scope.location1) {
+        var marker = {
           id: Date.now(),
           coords: {
             latitude: $scope.location1.latitude,
             longitude: $scope.location1.longitude
           }
         };
-        $scope.map.markers.push(marker1);
+        $scope.shuttle1 = marker;
       }
+    };
+    $scope.shuttle2 = {id: Date.now()};
 
-      if($scope.location2) {
-        var marker2 = {
+    $scope.updateShuttle2Map = function() {
+      if ($scope.location2) {
+        var marker = {
           id: Date.now(),
           coords: {
             latitude: $scope.location2.latitude,
             longitude: $scope.location2.longitude
           }
         };
-        $scope.map.markers.push(marker2);
+        $scope.shuttle2 = marker;
       }
+    };
+    $scope.shuttle3 = {id: Date.now()};
 
-      if($scope.location3) {
-        var marker3 = {
+    $scope.updateShuttle3Map = function() {
+      if ($scope.location3) {
+        var marker = {
           id: Date.now(),
           coords: {
             latitude: $scope.location3.latitude,
             longitude: $scope.location3.longitude
           }
         };
-        $scope.map.markers.push(marker3);
+        $scope.shuttle3 = marker;
       }
+    };
+    //Requests
+    $scope.stop.mz.to = '';
+    $scope.requestAtMZ = function() {
+      var request = {};
+      request.from = "MZ";
+      request.to = $scope.stop.mz.to;
+      $scope.sendRequest(request);
+      $scope.stop.mz.show = false;
+    };
 
+    $scope.requestAtCaltrain = function() {
+      var request = {};
+      request.from = "Caltrain";
+      request.to = "MZ";
+      $scope.sendRequest(request);
+      $scope.stop.caltrain.show = false;
+    };
+
+    $scope.requestAtHanover = function() {
+      var request = {};
+      request.from = "Hanover";
+      request.to = "MZ";
+      $scope.sendRequest(request);
+      $scope.stop.hanover.show = false;
+    };
+
+    $scope.sendRequest = function(request) {
+      var message = {from:request.from, to:request.to};
+      $scope.shuttleRequestChannel.publish(message);
     }
-
 
   });
