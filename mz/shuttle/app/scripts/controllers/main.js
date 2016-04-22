198f7eb40bf29e379cc91fb07826ac695d98f069
Om Muppirala <omuppirala@machinezone.com>
2016-04-22 09:13:41 -0700

    New icons

--- mz/shuttle/app/scripts/controllers/main.js
+++ mz/shuttle/app/scripts/controllers/main.js
@@ -18,11 +18,11 @@ angular.module('webappApp')
     $scope.map = {center: { latitude: 37.419472, longitude: -122.146881 },
                   zoom: 15,
                   markers: [],
-                  options:{icon: 'images/shuttle_icon.png'}
+                  options:{icon: 'images/shuttle_top.png'}
     };
 
     $scope.stop = {};
-    $scope.stop.options = {icon: 'images/StartPoint.png'};
+    $scope.stop.options = {icon: 'images/StartPointRed.png'};
     $scope.stop.mz =
       {
         coords: {latitude: 37.419472, longitude: -122.146881},
@@ -98,35 +98,53 @@ angular.module('webappApp')
 
     };
     $scope.location1;
+    $scope.prevLocation1;
 
     $scope.handleShuttle1Data = function(data)
     {
+      $scope.prevLocation1 = $scope.location1;
       $scope.location1 = data.body.messages[0];
       $timeout($scope.updateShuttle1Map,500);
     };
-    $scope.location2;
 
+
+    $scope.location2;
+    $scope.prevLocation2;
     $scope.handleShuttle2Data = function(data)
     {
+      $scope.prevLocation2 = $scope.location2;
       $scope.location2 = data.body.messages[0];
       $timeout($scope.updateShuttle2Map,500);
     };
-    $scope.location3;
 
+    $scope.location3;
+    $scope.prevLocation3;
     $scope.handleShuttle3Data = function(data)
     {
+      $scope.prevLocation3 = $scope.location3;
       $scope.location3 = data.body.messages[0];
       $timeout($scope.updateShuttle3Map,500);
     };
     $scope.shuttle1 = {id: Date.now()};
 
     $scope.updateShuttle1Map = function() {
+
+      if($scope.prevLocation1) {
+        var p1 = new LatLon($scope.prevLocation1.latitude, $scope.prevLocation1.longitude);
+        var p2 = new LatLon($scope.location1.latitude, $scope.location1.longitude);
+        var b2 = p1.finalBearingTo(p2);
+      }
+
       if ($scope.location1) {
         var marker = {
           id: Date.now(),
           coords: {
             latitude: $scope.location1.latitude,
             longitude: $scope.location1.longitude
+          },
+          options: {
+            icon: 'images/BusFrontIcon.png',
+            rotation: b2
           }
         };
         $scope.shuttle1 = marker;
@@ -135,12 +153,22 @@ angular.module('webappApp')
     $scope.shuttle2 = {id: Date.now()};
 
     $scope.updateShuttle2Map = function() {
+
+      if($scope.prevLocation2) {
+        var p1 = new LatLon($scope.prevLocation2.latitude, $scope.prevLocation2.longitude);
+        var p2 = new LatLon($scope.location2.latitude, $scope.location2.longitude);
+        var b2 = p1.finalBearingTo(p2);
+      }
       if ($scope.location2) {
         var marker = {
           id: Date.now(),
           coords: {
             latitude: $scope.location2.latitude,
             longitude: $scope.location2.longitude
+          },
+          options: {
+            icon: 'images/BusFrontIcon.png',
+            rotation: b2
           }
         };
         $scope.shuttle2 = marker;
@@ -149,12 +177,22 @@ angular.module('webappApp')
     $scope.shuttle3 = {id: Date.now()};
 
     $scope.updateShuttle3Map = function() {
+
+      if($scope.prevLocation3) {
+        var p1 = new LatLon($scope.prevLocation3.latitude, $scope.prevLocation3.longitude);
+        var p2 = new LatLon($scope.location3.latitude, $scope.location3.longitude);
+        var b2 = p1.finalBearingTo(p2);
+      }
       if ($scope.location3) {
         var marker = {
           id: Date.now(),
           coords: {
             latitude: $scope.location3.latitude,
             longitude: $scope.location3.longitude
+          },
+          options: {
+            icon: 'images/BusFrontIcon.png',
+            rotation: b2
           }
         };
         $scope.shuttle3 = marker;
