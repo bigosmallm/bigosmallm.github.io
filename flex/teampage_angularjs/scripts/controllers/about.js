b9ac407cf852c7efdb9a78f5bdc2ba9c1510c310
Om Muppirala <omuppirala@mz.com>
2016-12-24 00:17:37 -0800

    add angularjs version of apache flex team page

---
+++ flex/teampage_angularjs/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flexTeamPageApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the flexTeamPageApp
+ */
+angular.module('flexTeamPageApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ flex/teampage_angularjs/scripts/controllers/main.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flexTeamPageApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the flexTeamPageApp
+ */
+angular.module('flexTeamPageApp')
+  .controller('MainCtrl', function ($scope,$http) {
+
+    $scope.selectedFilter = 'a';
+    $scope.filteredTeamMembers = [];
+
+    $http.get('/scripts/data/team.json').then(function(response) {
+      $scope.teamMembers = response.data.members;
+      $scope.filteredTeamMembers = angular.copy($scope.teamMembers);
+    }, function(error) {
+      console.log(error);
+    });
+
+    $scope.handleGroup1Change = function() {
+      switch ($scope.selectedFilter) {
+        case 'a': {
+          resetFilters();
+          break;
+        }
+        case 'c': {
+          filterForCommitters();
+          break;
+        }
+        case 'p': {
+          filterForPMC();
+          break;
+        }
+        case 'm': {
+          filterForMentors();
+          break;
+        }
+        default: break;
+      }
+    };
+
+    function resetFilters() {
+      $scope.filteredTeamMembers = angular.copy($scope.teamMembers);
+    }
+
+    function filterForCommitters() {
+      $scope.filteredTeamMembers = $scope.teamMembers.filter(function (member) {
+        return (member.apacheID.indexOf('(C)') != -1);
+      });
+    }
+
+    function filterForPMC() {
+      $scope.filteredTeamMembers = $scope.teamMembers.filter(function (member) {
+        return (member.apacheID.indexOf('(C+P)') != -1);
+      });
+    }
+
+    function filterForMentors() {
+      $scope.filteredTeamMembers = $scope.teamMembers.filter(function (member) {
+        return (member.title === 'Project Mentor');
+      });
+    }
+
+  });
